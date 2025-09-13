2c8c5ec852acffef7113dbf482c8e1da652109a1
San1estus <j.rodriguezisiordia@gmail.com>
2025-09-13 10:18:48 -0600

    Implementacion Jarvis March
    Se implemento el primer algoritmo de Convex Hull

--- ConvexHulls.cpp
+++ ConvexHulls.cpp
@@ -1,5 +1,18 @@
 #include "geo.h"
+
 using namespace std;
+
+vector<point> randomPoints(int n){
+    vector<point>points;
+    for(int i = 0; i < n; i++){
+        double xrand = 100 * (double)rand()/(RAND_MAX+1);
+        double yrand = 100* (double)rand()/(RAND_MAX+1);
+
+        points.push_back({xrand, yrand});
+    }
+
+    return points;
+}
 
 inline bool isConvex(vector<point> &p){
     int n = p.size();
@@ -13,16 +26,44 @@ inline bool isConvex(vector<point> &p){
     return true;
 }
 
-int main(void){
-    vector<point> p;
-    point a(0.3, 0.3);
-    point b(1.5, 1);
-    point c(0.5, 0.5);
-    p.push_back(a);
-    p.push_back(b);
-    p.push_back(c);
-    p.push_back(a);
+vector<point> jarvisMarch(vector<point> &p){
+    int n = p.size();
+    if(n<=3) return p;
+    vector<point> CH;
+    int l = 0;
+    for(int i = 1; i < n; i++){
+        if(p[i].x < p[l].x || (p[i].x == p[l].x && p[i].y < p[l].y)) l = i;
+    }
+    int pivot = l, k;
+    do{
+        CH.push_back(p[pivot]);
+        k = (pivot+1)%n;
+        for(int i = 1; i < n; i++){
+            if(i!=pivot){
+                if(ccw(p[pivot], p[i], p[k])){
+                    k=i;
+                }
+            }
+        }
+        pivot = k;
+    }while(pivot!=l);
 
-    cout << isConvex(p) << '\n';
+    return CH;
+}
+
+int main(void){
+    srand(time(NULL));
+    int n;
+    cin >> n;
+    vector<point> p = randomPoints(n);
+
+    p.push_back(p[0]);
+
+    vector<point>CHp = jarvisMarch(p);
+    for(int i = 0; i < CHp.size(); i++){
+        cout << CHp[i].x << ' ' <<CHp[i].y <<'\n';
+    }
+    
+    cout << '\n' << isConvex(CHp);
     return 0;
 }
