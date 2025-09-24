430bfcf9dca01f284ececa800976b06c28291afa
San1estus <j.rodriguezisiordia@gmail.com>
2025-09-24 17:07:11 -0600

    Se añadio la carpeta de animaciones
    En esta carpeta se encuentra todo para construir y ejecutar las animaciones de los algoritmos

---
+++ Animaciones/JarvisMarch.cpp
@@ -0,0 +1,146 @@
+﻿#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include "geo.h"
+
+using namespace std;
+
+vector<point> randomPoints(int n) {
+    vector<point>points;
+    for (int i = 0; i < n; i++) {
+        float xrand = -0.9 + 1.5 * (float)rand() / (RAND_MAX + 1);
+        float yrand = -0.9 + 1.5 * (float)rand() / (RAND_MAX + 1);
+
+        points.push_back({ xrand, yrand });
+    }
+
+    return points;
+}
+
+vector<point> points;
+vector<point> hull;
+
+int pivot = -1;      // Punto actual del hull
+int bestIdx = -1;    // Mejor candidato actual
+int testIdx = -1;    // Índice en barrido
+bool finished = false;
+
+float lastTime = 0;
+
+void drawPoint(point p, float r, float g, float b, float size = 5.0f) {
+    glPointSize(size);
+    glColor3f(r, g, b);
+    glBegin(GL_POINTS);
+    glVertex2f(p.x, p.y ); 
+    glEnd();
+}
+
+void drawLine(point a, point b, float r, float g, float bl, float width = 1.0f) {
+    glLineWidth(width);
+    glColor3f(r, g, bl);
+    glBegin(GL_LINES);
+    glVertex2f(a.x , a.y);
+    glVertex2f(b.x, b.y);
+    glEnd();
+}
+
+void stepJarvisMarch(vector<point>& p) {
+    int n = (int)p.size();
+    if (finished || n <= 3) return; // n <=3 representa una linea o triangulo
+
+    if (hull.empty()) {
+        int l = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (p[i].x < p[l].x || (fabs(p[i].x - p[l].x) < EPS && p[i].y < p[l].y)) l = i;
+        }
+
+        pivot = l;
+        hull.push_back(p[pivot]);
+
+        bestIdx = (pivot + 1) % n;
+        testIdx = 0;
+        return;
+    }
+
+    if (testIdx != pivot) {
+        if (ccw(p[pivot], p[testIdx], p[bestIdx])) {
+            bestIdx = testIdx; 
+        }
+    }
+
+    testIdx++;
+
+    if (testIdx >= n) {
+        if (p[bestIdx] == hull[0]) {
+            finished = true;
+        }
+        else {
+            hull.push_back(p[bestIdx]);
+            pivot = bestIdx;
+
+            bestIdx = (pivot + 1) % n;
+            testIdx = testIdx % n;
+        }
+    }
+}
+
+void render() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    for (auto& p : points) drawPoint(p, 1, 0, 0);
+
+    if (hull.size() > 1) {
+        glColor3f(1, 1, 1);
+        glBegin(GL_LINE_STRIP);
+        for (auto& p : hull)
+            glVertex2f(p.x, p.y);
+        glEnd();
+    }
+
+    if (!finished && pivot != -1 && testIdx < points.size()) {
+        drawLine(points[pivot], points[testIdx], 1, 1, 0.5);
+        drawLine(points[pivot], points[bestIdx], 0, 1, 0, 2.0f); 
+    }
+
+    if (finished) {
+		drawLine(hull.back(), hull.front(), 1, 1, 1, 2.0f); 
+    }
+
+    for (auto& p : hull) drawPoint(p, 1, 1, 1, 8.0f);
+}
+
+int main() {
+    srand(time(NULL));
+	points = randomPoints(30);
+
+    if (!glfwInit()) return -1;
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Jarvis March", NULL, NULL);
+    if (!window) { glfwTerminate(); return -1; }
+
+    glfwMakeContextCurrent(window);
+    if (glewInit() != GLEW_OK) {
+        cout << "Error init GLEW\n"; return -1;
+    }
+
+    lastTime = glfwGetTime();
+    while (!glfwWindowShouldClose(window)) {
+
+        float now = glfwGetTime();
+
+        if (now - lastTime > 0.05) { 
+            stepJarvisMarch(points);
+            lastTime = now;
+        }
+
+        render();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
---
+++ Animaciones/geo.h
@@ -0,0 +1,94 @@
+#include <cmath>
+#include <vector>
+#include <iostream>
+using namespace std;
+#define EPS 1e-9
+
+struct point{
+    double x, y;
+    point() : x(0), y(0) {}
+    point(double x0, double y0) : x(x0), y(y0){}
+
+    point& operator+=(point o){
+        x += o.x;
+        y += o.y;
+        return *this;
+    }
+    point& operator-=(point o){
+        x -= o.x;
+        y -= o.y;
+        return *this;
+    }
+    point& operator*=(double o){
+        x *= o;
+        y *= o;
+        return *this;
+    }
+    point& operator/=(double o){
+        double div = 1.0/o;
+        x /= div;
+        y /= div;
+        return *this;
+    }
+
+    bool operator==(point o){
+        return (fabs(x-o.x) < EPS && fabs(y-o.y) < EPS);
+    }
+};
+
+inline point operator-(point p){
+    return point(-p.x,-p.y);
+}
+
+inline point operator-(point p, point q){
+    return point(p.x-q.x,p.y-q.y);
+}
+
+inline point operator+(point p, point q){
+    return point(p.x+q.x,p.y+q.y);
+}
+
+inline point operator*(point p, double esc){
+    return point(p.x*esc,p.y*esc);
+}
+
+inline point operator*(double esc, point p){
+    return point(p.x*esc,p.y*esc);
+}
+
+inline point operator/(point p, double esc){
+    esc = 1.0/esc;
+    return p*esc;
+}
+
+inline double cross(point p, point q){
+    return p.x*q.y-q.x*p.y;
+}
+
+inline double dot(point p, point q){
+    return p.x*q.x+p.y*q.y;
+}
+
+inline double sqnorm(point p){
+    return dot(p,p);
+}
+
+inline double dist(point p, point q){
+    return sqrt(sqnorm(p-q));
+}
+
+bool collinear(point p, point q, point r) {
+    return fabs(cross(p-q, p-r)) < EPS;
+}
+
+inline bool ccw(point p, point q, point r){
+    return cross(p-q,p-r) > 0;
+}
+
+inline void print(const vector<point> &p){
+    int n = (int)p.size();
+    for(int i = 0; i < n; i++){
+        cout << i+1 << ": " <<  p[i].x << ' ' << p[i].y << '\n';
+    }
+    cout << "\n\n";
+}
